aacb91e79132d9606d430e914ef4eca5476e8797
Sagar Kerhalkar <Sagarhatta@gmail.com>
2026-07-23 18:40:48 +0530

    Test editable Employee IDs and optional mobile numbers

--- inventory/tests/test_phase3.py
+++ inventory/tests/test_phase3.py
@@ -1,16 +1,23 @@
-from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from inventory.models import Employee, TshirtBrand, User
 
 
 class PhaseThreeModelTests(TestCase):
-    def test_employee_is_non_login_record_and_id_is_immutable(self):
+    def test_employee_is_non_login_record_and_id_can_be_corrected(self):
         employee = Employee.objects.create(employee_id="NXTTP0043", full_name="Employee", mobile_number="+919876543210", default_tshirt_size="L")
         self.assertFalse(hasattr(employee, "password"))
         employee.employee_id = "NXTTP0044"
-        with self.assertRaises(ValidationError):
-            employee.save()
+        employee.save()
+        employee.refresh_from_db()
+        self.assertEqual(employee.employee_id, "NXTTP0044")
+
+    def test_employee_mobile_number_is_optional(self):
+        first = Employee.objects.create(employee_id="NXTTP0045", full_name="Employee One", mobile_number=None)
+        second = Employee.objects.create(employee_id="NXTTP0046", full_name="Employee Two", mobile_number="")
+        self.assertIsNone(first.mobile_number)
+        second.refresh_from_db()
+        self.assertIsNone(second.mobile_number)
 
     def test_staff_role_is_displayed_as_data_entry_user(self):
         self.assertEqual(User.Role.STAFF.label, "Data Entry User")
