abef0d7b5cdf8a95116ccdb76709a3b33ecd3f9c
Sagar Kerhalkar <Sagarhatta@gmail.com>
2026-07-23 15:58:33 +0530

    Set minimum login password length to four characters

--- nexttoppers_inventory/settings.py
+++ nexttoppers_inventory/settings.py
@@ -96,10 +96,7 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", "OPTIONS": {"min_length": 10}},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", "OPTIONS": {"min_length": 4}},
 ]
 AUTH_USER_MODEL = "inventory.User"
 
