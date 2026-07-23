5105deb4bc157105e0850e60ed189c01b72a3f86
Sagar Kerhalkar <Sagarhatta@gmail.com>
2026-07-23 18:40:36 +0530

    Update model tests for editable IDs

--- inventory/tests/test_models.py
+++ inventory/tests/test_models.py
@@ -1,14 +1,16 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
+
 from inventory.models import Book, User
 
 
 class UserModelTests(TestCase):
-    def test_employee_id_is_immutable(self):
+    def test_login_user_id_can_be_corrected(self):
         user = User.objects.create_user("NXTTP0001", "Test User", "+919876543210", "StrongPass123!")
         user.employee_id = "NXTTP0002"
-        with self.assertRaises(ValidationError):
-            user.save()
+        user.save()
+        user.refresh_from_db()
+        self.assertEqual(user.employee_id, "NXTTP0002")
 
     def test_mobile_must_be_unique(self):
         User.objects.create_user("NXTTP0001", "One", "+919876543210", "StrongPass123!")
@@ -23,6 +25,13 @@ class BookModelTests(TestCase):
     def test_asset_id_is_generated(self):
         book = Book.objects.create(name="Physics", created_by=self.user)
         self.assertRegex(book.asset_id, r"^BOOK\d{6}$")
+
+    def test_asset_id_can_be_corrected(self):
+        book = Book.objects.create(asset_id="OLD-001", name="Physics", created_by=self.user)
+        book.asset_id = "NEW-001"
+        book.save()
+        book.refresh_from_db()
+        self.assertEqual(book.asset_id, "NEW-001")
 
     def test_book_with_history_cannot_be_deleted(self):
         book = Book.objects.create(name="Physics", created_by=self.user, condition=Book.Condition.LOST)
