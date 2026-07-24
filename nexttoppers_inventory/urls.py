869be0cbc946340d6f50e99ba9a4467b615c3f52
Sagar Kerhalkar <Sagarhatta@gmail.com>
2026-07-24 13:34:48 +0530

    Exempt nonce login callback from Django CSRF middleware

--- nexttoppers_inventory/urls.py
+++ nexttoppers_inventory/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from django.middleware.csrf import get_token
 from django.urls import include, path
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 from .auth_views import InternalNonceLoginView
@@ -31,7 +32,7 @@ urlpatterns = [
     path("health/session-csrf-token/", session_csrf_token, name="session_csrf_token"),
     path("health/session-csrf-probe/", session_csrf_probe, name="session_csrf_probe"),
     path("admin/", admin.site.urls),
-    path("login/", InternalNonceLoginView.as_view(), name="login"),
+    path("login/", csrf_exempt(InternalNonceLoginView.as_view()), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", include("inventory.urls")),
 ]
