fa6e48defbd1cf99ba6b75e1b8e5e65ae55ed370
Sagar Kerhalkar <Sagarhatta@gmail.com>
2026-07-24 12:44:38 +0530

    Add proxy-free IIS health endpoint

--- nexttoppers_inventory/urls.py
+++ nexttoppers_inventory/urls.py
@@ -2,9 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.http import HttpResponse
 from django.urls import include, path
 
+
+def health_check(request):
+    """Lightweight local/IIS health endpoint with no template or database work."""
+    return HttpResponse("NEXT_TOPPERS_INVENTORY_OK", content_type="text/plain")
+
+
 urlpatterns = [
+    path("health/", health_check, name="health_check"),
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
